44b4cbb2ee527e1480fb30bbe2d5204ca11facfb
Andres <andresdreamerf@gmail.com>
2022-02-11 18:49:08 -0400

    tercer commit

--- codigo.js
+++ codigo.js
@@ -1,5 +1,11 @@
 
 	let boton = document.getElementById('boton')
+	let contenedor = document.querySelector('.contenedor')
+
 	boton.addEventListener('click',()=>{
 		alert('clickado');
+		
+	let h1 = document.createElement('h1')
+		h1.innerHTML = `<h1>Nuevo titulo</h1>`;
+		contenedor.append(h1);
 	})
