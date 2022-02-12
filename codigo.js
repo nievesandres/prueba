4422350d393346f59a86aacbeee2826ec1bce9cf
Andres <andresdreamerf@gmail.com>
2022-02-12 16:37:04 -0400

    nueva funcionalidad en feature

--- codigo.js
+++ codigo.js
@@ -7,4 +7,8 @@
 		contenedor.insertAdjacentHTML('beforeend',`
 				<h1>Nuevo titulo con insertAdjacent</h1>
 		`);
+		let newContenedor = document.createElement('div');
+		newContenedor.className = 'newContenedor';
+		newContenedor.innerHTML = '<h1>RANCIO</h1>'
+		contenedor.after(newContenedor);
 	})
